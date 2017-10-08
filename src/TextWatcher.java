e915b29fec45c79729ca4ce0f026cf0717f998f1
absltn <miet.kremerov@gmail.com>
2017-10-08 18:00:50 +0300

    добавил окончания глаголов

--- src/TextWatcher.java
+++ src/TextWatcher.java
@@ -13,9 +13,11 @@ public class TextWatcher {
         String content = null;
         content = readUsingFiles(fileName);
         String [] words = content.split ("([\\s/.,]+)");
+
         for (int i=0; i<words.length;i++)
-            if (checkForAdj(words[i]) )
+            if (checkForAdj(words[i]) ) 
                 result[0]++;
+
             else if (checkForVerb(words[i]))
                 result[1]++;
             else if (checkForAdverb(words[i]))
@@ -40,14 +42,15 @@ public class TextWatcher {
         return madj.matches();
     }
     public static boolean checkForVerb(String userNameString) {
-        Pattern verb = Pattern.compile("[а-яА-Я]{2,}((ти)|([ауоиыея]ть)|([ауоиыея]л[аои]{1})$)");
+        Pattern verb = Pattern.compile("[а-яА-Я]{2,}((ти)|([ауоиыея][ть])|([ияюе]т)|([(ьте)(ай)(рь)(ите)])|([ие](шь)|(те)|м)|([ия]л[аои]{1})$)");
         Matcher mverb = verb.matcher(userNameString);
         return mverb.matches();
     }
 
     public static boolean checkForAdverb(String userNameString){
-        Pattern adverb = Pattern.compile("[освнпзтудегчб][а-яА-Я]{2,}(([елиз(яр)ь](ко))|(ро)|(вчера)|(сегодня)|" +
-                "(завтра)|(сь)|(назад)|(вперед)|([ае][(во),(ку)])|(перь)|([дт]ан)|(ину)|([еяу]ча)$)");
+        Pattern adverb = Pattern.compile("[оОсСвВнНпПзЗтТуУдДеЕгГчЧбБ][а-яА-Я]{1,}" +
+                "(([елиз(яр)ь](ко))|(ро)|(вчера)|(сегодня)|" +
+                "(завтра)|(сь)|(назад)|(вперед)|([ае][(во),(ку)])|(перь)|([лрн][о(ее)])|(ив[о(ее)])|(ев[о(ее)])|([дт]ан)|(ину)|([еяу]ча)$)");
         Matcher madverb = adverb.matcher(userNameString);
         return madverb.matches();
     }
