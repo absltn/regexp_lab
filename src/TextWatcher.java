23ce3e374445bd0e37e9a1a58bac4a5d3cf754ba
absltn <miet.kremerov@gmail.com>
2017-10-08 17:38:28 +0300

    как же тормозит IntelliJ Idea на этом компьютере. Еле удалось допилить чтение из файла.

--- src/TextWatcher.java
+++ src/TextWatcher.java
@@ -1,19 +1,39 @@
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.lang.String;
 
 public class TextWatcher {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Results of checking:");
-        String userInput;
-
-        System.out.println(checkForAdj("хорошему"));
-        System.out.println(checkForAdj("волчая"));
-        System.out.println(checkForAdj("волчий"));
-        System.out.println(checkForAdj("настоящей"));
-        System.out.println(checkForVerb("стоить"));
-        System.out.println(checkForAdverb("вголосину"));
+        String fileName = "C:/users/airliq/desktop/regTest.txt";
+        int result[]={0,0,0};
+        String content = null;
+        content = readUsingFiles(fileName);
+        String [] words = content.split ("([\\s/.,]+)");
+        for (int i=0; i<words.length;i++)
+            if (checkForAdj(words[i]) )
+                result[0]++;
+            else if (checkForVerb(words[i]))
+                result[1]++;
+            else if (checkForAdverb(words[i]))
+                result[2]++;
+        System.out.printf("В данном тексте примерно %d прилагательных, %d глаголов и " +
+                "%d наречий.",result[0],result[1],result[2]);
     }
-    public static boolean checkForAdj(String userNameString) {
+
+    private static String readUsingFiles(String fileName) throws IOException {
+        try{
+            Files.readAllBytes(Paths.get(fileName));}
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+
+     public static boolean checkForAdj(String userNameString) {
         Pattern adj = Pattern.compile("[а-яА-Я]{3,}(([оиеы]й)|([ео]е|м|(му)|(го))|([аиья]я)|" +
                 "((ле)|(не)|(ше)|(ще)|(че)й)$)");
         Matcher madj = adj.matcher(userNameString);
